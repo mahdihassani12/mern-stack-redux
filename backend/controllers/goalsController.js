6111f54f483d5440cde03edaae132210943dbc7f
Mahdi Hassai <hassanim430@gmail.com>
2023-08-22 20:59:17 +0430

    adding express async handler

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,26 +1,28 @@
+import asyncHandler from 'express-async-handler'; 
+
 // returning all goals from database
-const getGoals = async (req, res, next) => {
+const getGoals = asyncHandler( async (req, res, next) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // get a goal by id
-const getGoalById = async (req, res, next) => {
+const getGoalById = asyncHandler( async (req, res, next) => {
   res.status(200).json({ message: `Get a goal ${req.params.id}` });
-};
+});
 
 // storing goals into database
-const createGoal = async (req, res, next) => {
+const createGoal = asyncHandler( async (req, res, next) => {
   res.status(200).json({ message: "Create a goal" });
-};
+});
 
 // updating goals
-const updateGoal = async (req, res, next) => {
+const updateGoal = asyncHandler( async (req, res, next) => {
   res.status(200).json({ message: `Update a goal ${req.params.id}` });
-};
+});
 
 // deleting goals
-const DeleteGoal = async (req, res, next) => {
+const DeleteGoal = asyncHandler( async (req, res, next) => {
   res.status(200).json({ message: `Delete a goal ${req.params.id}` });
-};
+});
 
 export { getGoals, createGoal, updateGoal, DeleteGoal, getGoalById };
