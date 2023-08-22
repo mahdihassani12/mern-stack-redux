9ad3e3362f04c090a184478a73e76cf1d3c8ef79
Mahdi Hassai <hassanim430@gmail.com>
2023-08-22 21:20:14 +0430

    crud operation on database

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,28 +1,56 @@
-import asyncHandler from 'express-async-handler'; 
+import asyncHandler from "express-async-handler";
+import Goal from "../models/goal.js";
 
 // returning all goals from database
-const getGoals = asyncHandler( async (req, res, next) => {
-  res.status(200).json({ message: "Get goals" });
+const getGoals = asyncHandler(async (req, res, next) => {
+  const goals = await Goal.find({});
+  res.status(200).json(goals);
 });
 
 // get a goal by id
-const getGoalById = asyncHandler( async (req, res, next) => {
-  res.status(200).json({ message: `Get a goal ${req.params.id}` });
+const getGoalById = asyncHandler(async (req, res, next) => {
+  const goal = await Goal.findById(req.params.id);
+  if (goal) return res.json(goal);
+  res.status(404);
+  throw new Error("Resource not found");
 });
 
 // storing goals into database
-const createGoal = asyncHandler( async (req, res, next) => {
-  res.status(200).json({ message: "Create a goal" });
+const createGoal = asyncHandler(async (req, res, next) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text value");
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 // updating goals
-const updateGoal = asyncHandler( async (req, res, next) => {
-  res.status(200).json({ message: `Update a goal ${req.params.id}` });
+const updateGoal = asyncHandler(async (req, res, next) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+
+  const updatedGoal = await Goal.updateOne({_id: req.params.id }, { text: req.body.text });
+  res.status(200).json(updatedGoal);
 });
 
 // deleting goals
-const DeleteGoal = asyncHandler( async (req, res, next) => {
-  res.status(200).json({ message: `Delete a goal ${req.params.id}` });
+const DeleteGoal = asyncHandler(async (req, res, next) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+
+  const deletedGoad = await Goal.findOneAndDelete({_id: req.params.id });
+  res.send(post);
+  res.status(200).json(deletedGoad);
 });
 
 export { getGoals, createGoal, updateGoal, DeleteGoal, getGoalById };
---
+++ backend/models/goal.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: ["Please add a text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Goal = mongoose.model("Goal", goalSchema);
+export default Goal;
