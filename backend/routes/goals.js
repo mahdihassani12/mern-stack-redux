22bfae9d761c4bf18c62460779a926d938aea0b4
Mahdi Hassai <hassanim430@gmail.com>
2023-08-22 18:09:50 +0430

    making crud routes

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Get goals" });
 });
 
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Create a goal" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update a goal ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete a goal ${req.params.id}` });
+});
+
 export default router;
